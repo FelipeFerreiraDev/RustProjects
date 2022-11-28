91266360515843f4264020fc90e0c3407ef42d5e
Felipe Henrique Camargos Ferreira <felipeferreira.sp.dev@gmail.com>
2022-11-28 17:39:57 -0300

    primeiro projeto com rust

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let name = "Felipe"; // String literal
+    let mut age = 28; // Integer literal with mutation
+    let is_active = true; // Boolean literal
+    age = 20;
+    println!("Hello {}! {} anos, está {}", name, age, is_active); // <- 
+}
